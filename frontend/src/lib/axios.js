dcfcede423eb48be8a1edbd8e84bc9abc66418ec
Smruti Ranjan Sahoo <68041717+Smruti-Ranjan-Sahoo-Tech@users.noreply.github.com>
2026-01-03 22:00:42 +0530

    initial server start 3

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: "https://smartdev-the-ai-code-reviewer.onrender.com",
+    baseURL: "http://localhost:3000",
     withCredentials:true,
 })
